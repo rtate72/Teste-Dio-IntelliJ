99ff48ea7c91ca74a2fdbe4feb681741d25d043f
Rtate72 <rtate@terra.com.br>
2022-10-10 18:38:55 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.modal.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+/*        int a = 5;
+                                      int b = 3;
+
+
+        System.out.println("Hello World! " + (a+b));*/
+
+    }
+}
